044929f717bd23f54b9368de991d1dadae49da2a
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-09-20 19:12:50 +0200

    init

---
+++ GradeFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+// import an external contract into this contract
+import "./GradeStorage.sol";
+
+// this contract inherits the imported contract
+contract GradeFactory is GradeStorage {
+
+    // array is created to store the contracts
+    GradeStorage[] public gradeStorageArray;
+    
+    // public function to deploy the grade storage contract
+    // object of the grade storage contract created with no params
+    // functions adds to the grade storage array
+    function createGradeFactory() public {
+        GradeStorage gradeStorage = new GradeStorage();
+        gradeStorageArray.push(gradeStorage);
+    }
+    
+    // public function to store a grade on a contract
+    function storeGradeFactory(uint256 _index, uint256 _grade) public {
+        GradeStorage(address(gradeStorageArray[_index])).storeGrade(_grade);
+    }
+    
+    // function allows the entering of an index and returns the grade
+    function getGradeFactory(uint256 _index) public view returns (uint256) {
+        return GradeStorage(address(gradeStorageArray[_index])).readGrade();
+    }
+}
